c623d079eb308c0641eac0351a2c6e2525236426
Derek Song <dsong1@stuy.edu>
2018-10-03 06:41:29 -0400

    k15

--- 15_flashinh/test.py
+++ 15_flashinh/test.py
@@ -1,9 +1,9 @@
 #Team Derek - Derek Chan and Derek Song
 #SoftDev1 pd8
-#K14 -- Do I Know You?
-#2018-10-02
+#K15 -- Oh yes, perhaps I do…
+#2018-10-03
 import os
-from flask import Flask, render_template, request, session, redirect, url_for
+from flask import Flask, render_template, request, session, redirect, url_for, flash
 app = Flask(__name__)
 
 app.secret_key = os.urandom(32) #set the secret key
@@ -18,15 +18,18 @@ def login():
     if request.form['username'] in test_user: #sees if the username is correct
         if request.form['password'] == test_user['chan']: #sees if the password for the username is correct
             session[request.form['username']] = request.form['password'] #creates the session only if password and username are correct
-            return render_template('welcome.html', user = request.form['username']) #returns a welcome if password and username are correct
-        return render_template('denied.html', user = request.form['username'], error = 'has a incorrect password') #returns a password error if only the username is recognized
-    return render_template('denied.html', user = request.form['username'], error = 'is unrecognized') #everything is wrong
+            flash('Welcome '+ request.form['username'] +', how are you?')
+            return render_template('welcome.html', title = 'Welcome') #returns a welcome if password and username are correct
+        flash('Sorry '+ request.form['username'] +', has a incorrect password')
+        return render_template('welcome.html', title = 'ERROR') #returns a password error if only the username is recognized
+    flash('Sorry '+ request.form['username'] +', is unrecognized')
+    return render_template('welcome.html', title = 'ERROR') #everything is wrong
 
 @app.route("/logout")
 def logout():
     if 'chan' in session:
         session.pop('chan') #ends the session and redirects back to login page
-    return redirect(url_for("index")) #goes back to the login page 
+    return redirect(url_for("index")) #goes back to the login page
 
 @app.route("/test") #used to test if the session is remembered
 def test():
