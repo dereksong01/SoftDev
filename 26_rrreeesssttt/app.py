7fffed1ac7484567dd8fb6151d91eccbdce8a64d
dereksong01 <dsong1@stuy.edu>
2018-11-16 07:53:18 -0500

    reorganize

--- 26_rrreeesssttt/app.py
+++ 26_rrreeesssttt/app.py
@@ -1,7 +1,7 @@
 # Derek Song
 # SoftDev1 pd8
-# K25 -- Getting More REST
-# 2018 - 11 - 15
+# K26 -- Getting More REST
+# 2018 - 11 - 16
 
 from flask import Flask, render_template
 import urllib, json
@@ -9,14 +9,71 @@ import urllib, json
 app = Flask(__name__)
 
 @app.route("/")
-def inform_user():
-    KEY_NDB = "hjmklcFuCL8zkX8MTENIODUAZGPFgTlddwDJxGZ2" # API Key
-    URL_STUB = "https://developer.nrel.gov/api/alt-fuel-stations/v1/nearest.json?api_key=" # USDA
-    ext = "&location=Denver+CO" # Extension for example URL
-    url = URL_STUB + KEY_NDB + ext
-    print(url)
-    #x = urllib.request.urlopen('https://api.nasa.gov/planetary/apod?api_key=motrt4z0moLTYLk3ziqhyD8KCZsf6k4n8EwJcBN0').read()
-    return render_template('index.html')
+def home():
+
+    #oxford dictionary
+    app_id='de9afc70'
+    app_key='fe2edc1a976f63e88441f47ae69a3fb5'
+    language='en'
+    word='toxic'
+
+    # accessing and reading api
+    url = 'https://od-api.oxforddictionaries.com:443/api/v1/entries/'  + language + '/'  + word.lower()
+    u = requests.get(url, headers={'app_id':app_id, 'app_key':app_key})
+
+    #converting the json object string into a dict
+    info = json.loads(u.text)
+    print ("-----OXFORD------")
+    print (info)
+    print ("-----------------")
+
+    word=info["results"][0]["id"],
+    definition=info["results"][0]["lexicalEntries"][0]["entries"][0]['senses'][0]['definitions'][0]
+
+    #MTA
+
+    # accessing and reading api
+    url = 'http://mtaapi.herokuapp.com/stop?id=120S'
+    u = urllib.request.urlopen(url)
+
+    #converting the json object string into a dict
+    info = json.loads(u.read())
+    print ("-----MTA------")
+    print (info)
+    print ("-----------------")
+
+    station = info['result']['name']
+    latitude = info['result']['lat']
+    longitude = info['result']['lon']
+
+    #THE MET
+
+    # accessing and reading api
+    url = 'https://collectionapi.metmuseum.org/public/collection/v1/objects/1865'
+    u = urllib.request.urlopen(url)
+
+    #converting the json object string into a dict
+    info = json.loads(u.read())
+    print ("-----THE MET------")
+    print (info)
+    print ("-----------------")
+
+    title = info['title']
+    picture = info['primaryImage']
+    location = info['department']
+
+    #passing in api values to template
+    return render_template("index.html",
+                           word=word[0],
+                           definition=definition,
+                           station=station,
+                           latitude=latitude,
+                           longitude=longitude,
+                           title=title,
+                           picture=picture,
+                           location=location,
+                           )
+
 
 if __name__ == "__main__":
     app.debug = True
