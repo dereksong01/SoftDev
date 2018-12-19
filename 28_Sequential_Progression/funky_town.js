47d3fefce4914c965c78897b7e8cafd33660eee2
dereksong01 <dsong1@stuy.edu>
2018-12-19 06:46:22 -0500

    k28

---
+++ 28_Sequential_Progression/funky_town.js
@@ -0,0 +1,33 @@
+//DSONG AND AD
+
+var fibonacci = (n) => {
+  if (n == 0)
+    return 0;
+  else if (n == 1)
+    return 1;
+  else
+    return fibonacci(n - 2) + fibonacci(n - 1);
+};
+
+var gcd = (a, b) => {
+  if (a > b) {
+    for (x = b; x > 0; x--)
+      if (((a % x) == 0) && ((b % x) == 0))
+        return x;
+  }
+  else {
+    for (x = a; x > 0; x--)
+      if (((a % x) == 0) && ((b % x) == 0))
+        return x;
+  }
+}
+
+var students = ["bob", "sally", "john", "john2"];
+
+var randomNumber = () => {
+  return Math.random() * 3
+}
+var randomStudent = () => {
+  randIndex = randomNumber();
+  return students[randIndex];
+}
